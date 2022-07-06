1b99284d09b1320f6f54099b93ab9321d582dd56
kebaq <iqblshh34@gmail.com>
2022-07-06 08:06:40 +0800

    ctk

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import tkinter
-from turtle import title
 from customtkinter import *
 import sqlite3 as sl
 
 root = CTk()
 root.title('POS')
 root.iconbitmap('C:/Users/kebaq/Documents/GitHub/POS-system/ceriatoys.ico')
-root.geometry('1000x1000')
+root.geometry('500x500')
 
 
 root.mainloop()
