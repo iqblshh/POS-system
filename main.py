ccc85f425040918eb23ea7ec7f449dd826df5bd1
kebaq <iqblshh34@gmail.com>
2022-07-06 08:00:21 +0800

    icon added

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import tkinter
+from turtle import title
 from customtkinter import *
 import sqlite3 as sl
 
 root = CTk()
+root.title('POS')
+root.iconbitmap('C:/Users/kebaq/Documents/GitHub/POS-system/ceriatoys.ico')
+root.geometry('1000x1000')
+
+
 root.mainloop()
